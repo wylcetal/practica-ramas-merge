b4b7a59c50def013c85ad38f7d95150095a94ac5
WilliamBG Code <wylcetal@gmail.com>
2025-04-10 11:30:31 -0500

    Solucion 2 al ejercicio de colocal un boton deshacer

--- script.js
+++ script.js
@@ -11,3 +11,9 @@ function calcular() {
 function limpiarPantalla() {
     document.getElementById('pantalla').value = '';
 }
+
+function deshacer() {
+    const valorPantalla = document.getElementById('pantalla').value;
+    const resultado = valorPantalla.substring(0, valorPantalla.length - 1);
+    document.getElementById('pantalla').value = resultado;
+}
